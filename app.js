90a4981350714e8666e2c96a88618e3011da378b
gferreira <gferreiraa@gmail.com>
2019-03-26 00:30:51 -0300

    Mongoose config

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
  const express = require('express')
  const app = express()
 
+ const  mongoose = require('mongoose');
+ const bodyParser = require('body-parser')
+
+ // Mongoose
+ const url = 'mongodb+srv://usuario_admin:12345@cluster0-avzlp.mongodb.net/test?retryWrites=true'
+ const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true }
+
+ mongoose.connect(url, options)
+ mongoose.set('useCreateIndex, true')
+
+ mongoose.connection.on('error', (err) => {
+   console.log('Erro na conexão com o banco de dados: ' + err)
+ })
+
+ mongoose.connection.on('disconnected', () => {
+   console.log('Aplicação desconectada do banco de dados!')
+ })
+
+mongoose.connection.on('connected', () => {
+  console.log('Aplicação conectada ao banco de dados')
+})
+
+// BodyParser
+ app.use(bodyParser.urlencoded({ extended: false}))
+ app.use(bodyParser.json())
+
  const indexRoute = require('./Routes/index')
  const userRoute = require('./Routes/users')
 
