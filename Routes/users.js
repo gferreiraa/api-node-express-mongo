11db5262147282ba697941f322f180d3c8ba020c
gferreira <gferreiraa@gmail.com>
2019-03-27 22:23:56 -0300

    Router auth

--- Routes/users.js
+++ Routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 const Users = require('../model/user')
 
@@ -16,7 +17,7 @@ router.post('/create', (req, res) => {
 
   Users.findOne({ email }, (err, data) => {
     if(err) return res.send({ error: 'Erro ao buscar usuário' })
-    if(data) return res.send({ error: 'Usuário já resgistrado' })
+    if(data) return res.send({ error: 'Usuário já registrado' })
     
     Users.create(req.body, (err, data) => {
       if(err) return res.send({ error: 'Erro ao criar usuário' })
@@ -25,6 +26,23 @@ router.post('/create', (req, res) => {
         return res.send(data)
     })
   })
+})
+
+router.post('/auth', (req, res) =>{
+  const { email, password } = req.body
+  if(!email || !password) return  res.send({ error: 'Dados insuficientes!' })
+
+  Users.findOne({email}, (err, data) => {
+    if(err) return res.send({ error: 'Erro ao buscar usuário!' })
+    if(!data) return res.send({ error: 'Usuário  não registrado!'})
+
+    bcrypt.compare(password, data.password, (err, same) => {
+      if(!same) return res.send({ error: 'Erro ao autenticar usuário!'})
+
+      data.password = undefined
+      return res.send(data)
+    })
+  }).select('+password')
 })
 
 module.exports = router
